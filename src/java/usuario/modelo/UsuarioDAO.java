30dbb2e1919eb196168ad944e421a0b98a852fca
Cindy Damasceno <cindydamasceno@MacBook-Air-de-Cindy.local>
2022-10-15 12:49:11 -0300

    Model (MVC) completo

--- src/java/usuario/modelo/UsuarioDAO.java
+++ src/java/usuario/modelo/UsuarioDAO.java
@@ -4,9 +4,11 @@
  */
 package usuario.modelo;
 
+import categoria.modelo.Categoria;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 /**
@@ -15,8 +17,71 @@ import java.sql.SQLException;
  */
 public class UsuarioDAO {
     
-    public void inserir(String nome, String endereco, String email, String login, String senha) throws SQLException{
-        int resultado = 0;
+    public Usuario obter(int id) throws SQLException{
+        Usuario usuario = null;
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/smd_ecommerce_", "jeff", "jeff123"); 
+                
+                PreparedStatement preparedStatement = connection.prepareStatement("Select * usuario where id = ?")) {
+                preparedStatement.setInt(1, id);
+                
+                ResultSet resultSet = preparedStatement.executeQuery();
+                    
+                while(resultSet.next()){
+                    usuario = new Usuario();
+                    usuario.setId(resultSet.getInt("id"));
+                    usuario.setAdministrador(resultSet.getBoolean("administrador"));
+                    usuario.setNome(resultSet.getString("nome"));
+                    usuario.setEmail(resultSet.getString("email"));
+                    usuario.setEndereco(resultSet.getString("endereco"));
+                    usuario.setLogin(resultSet.getString("login"));
+                    usuario.setSenha(resultSet.getString("senha"));
+                }
+            }
+        }
+        catch (ClassNotFoundException ex) {
+            throw new SQLException(ex.getMessage());
+        }
+        
+        return usuario;
+    }
+    
+    public Usuario obter(String login) throws SQLException{
+        Usuario usuario = null;
+        
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/smd_ecommerce_", "jeff", "jeff123"); 
+                
+                PreparedStatement preparedStatement = connection.prepareStatement("Select * usuario where login = ?")) {
+                preparedStatement.setString(1, login);
+                
+                ResultSet resultSet = preparedStatement.executeQuery();
+                    
+                while(resultSet.next()){
+                    usuario = new Usuario();
+                    usuario.setId(resultSet.getInt("id"));
+                    usuario.setAdministrador(resultSet.getBoolean("administrador"));
+                    usuario.setNome(resultSet.getString("nome"));
+                    usuario.setEmail(resultSet.getString("email"));
+                    usuario.setEndereco(resultSet.getString("endereco"));
+                    usuario.setLogin(resultSet.getString("login"));
+                    usuario.setSenha(resultSet.getString("senha"));
+                }
+            }
+        }
+        catch (ClassNotFoundException ex) {
+            throw new SQLException(ex.getMessage());
+        }
+        
+        return usuario;
+    }
+    
+    public boolean inserir(String nome, String endereco, String email, String login, String senha) throws SQLException{
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             try (
@@ -27,17 +92,43 @@ public class UsuarioDAO {
                 preparedStatement.setString(3, email);
                 preparedStatement.setString(4, login);
                 preparedStatement.setString(5, senha);
-                resultado = preparedStatement.executeUpdate();
+                return preparedStatement.execute();
             }
         } catch (ClassNotFoundException ex) {
             throw new SQLException(ex.getMessage());
         }
-        if (resultado == 0) {
-            throw new SQLException("Registro não foi inserido com sucesso");
+    }
+    
+    public boolean atualizar(int id, String nome, String endereco, String email, String login, String senha) throws SQLException{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/smd_ecommerce_", "jeff", "jeff123"); 
+                PreparedStatement preparedStatement = connection.prepareStatement("UPDATE usuario SET nome = ?, endereco = ?, email = ?, login = ?, senha = ? WHERE id = ?")) {
+                preparedStatement.setString(1, nome);
+                preparedStatement.setString(2, endereco);
+                preparedStatement.setString(3, email);
+                preparedStatement.setString(4, login);
+                preparedStatement.setString(5, senha);
+                preparedStatement.setInt(6, id);
+                return preparedStatement.execute();
+            }
+        } catch (ClassNotFoundException ex) {
+            throw new SQLException(ex.getMessage());
         }
     }
-    public Usuario obter(int id){}
-    public Usuario obter(String login){}
-    public Usuario atualizar(int id, String nome, String endereco, String email, String login, String senha){}
-    public String remover(int id){}
+    
+    public boolean remover(int id) throws SQLException{
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/smd_ecommerce_", "jeff", "jeff123"); 
+                PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM usuario where id = ?")) {
+                preparedStatement.setInt(1, id);
+                return preparedStatement.execute();
+            }
+        } catch (ClassNotFoundException ex) {
+            throw new SQLException(ex.getMessage());
+        }
+    }
 }
