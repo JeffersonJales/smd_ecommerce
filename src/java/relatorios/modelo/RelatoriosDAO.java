257b9c2f77929ccdd3cfb130cfd6a795bc1486c7
JeffersonJales <jeffersonjales@hotmail.com>
2022-12-01 03:14:08 -0300

    Relatório Cliente

--- src/java/relatorios/modelo/RelatoriosDAO.java
+++ src/java/relatorios/modelo/RelatoriosDAO.java
@@ -53,12 +53,12 @@ public class RelatoriosDAO {
         List<RelatorioFaturamento> rfs = new ArrayList();
         
         String sql = 
-        "WITH tab1 as (select CAST(v.data_hora as DATE) as dia, p.descricao, " +
-	"p.preco, pv.quantidade, (pv.quantidade * p.preco) as total_preco from venda v " +
-        "inner join produto_venda pv on pv.id_venda = v.id " +
-        "inner join produto p on p.id = pv.id_produto " + 
-        "where v.data_hora >= ? AND  v.data_hora <= ?) " +
-        "select dia, sum(total_preco) as valor from tab1 group by dia order by dia;";
+            "WITH tab1 as (select CAST(v.data_hora as DATE) as dia, p.descricao, " +
+            "p.preco, pv.quantidade, (pv.quantidade * p.preco) as total_preco from venda v " +
+            "inner join produto_venda pv on pv.id_venda = v.id " +
+            "inner join produto p on p.id = pv.id_produto " + 
+            "where v.data_hora >= ? AND  v.data_hora <= ?) " +
+            "select dia, sum(total_preco) as valor from tab1 group by dia order by dia;";
         
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -84,5 +84,40 @@ public class RelatoriosDAO {
         return rfs;
     }
 
-    
+    public List<RelatorioCliente> Cliente(Date inicio, Date fim) throws SQLException{
+        List<RelatorioCliente> rcs = new ArrayList();
+        
+        String sql = 
+            "WITH tab1 as (select CAST(v.data_hora as DATE) as dia, p.descricao, p.preco, pv.quantidade, " +
+            "(pv.quantidade * p.preco) as total_preco, u.id, u.nome from venda v " +
+            "inner join usuario u on u.id = v.id_usuario " +
+            "inner join produto_venda pv on pv.id_venda = v.id " +
+            "inner join produto p on p.id = pv.id_produto " +
+            "where v.data_hora >= ? AND  v.data_hora <= ?)" +
+            "select id, nome, sum(quantidade) as total_compra from tab1 group by id order by total_compra desc;";
+        
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            try (
+                Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/smd_ecommerce_", "jeff", "jeff123"); 
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+                preparedStatement.setDate(1, inicio);
+                preparedStatement.setDate(2, fim);
+
+                ResultSet resultSet = preparedStatement.executeQuery();
+                
+                while(resultSet.next()){
+                    RelatorioCliente rc = new RelatorioCliente();
+                    rc.getUsuario().setId(resultSet.getInt("id"));
+                    rc.getUsuario().setNome(resultSet.getString("nome"));
+                    rc.setQuantidadeCompras(resultSet.getInt("total_compra"));
+                    rcs.add(rc);
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            throw new SQLException(ex.getMessage());
+        }
+        
+        return rcs;
+    }
 }
